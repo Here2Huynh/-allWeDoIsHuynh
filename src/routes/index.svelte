a29ac60af6a17166826d1f64afeef42384b089a0
John Huynh <jhnhnyh9@gmail.com>
2022-03-21 21:53:45 -0700

    feature(misc): added image backdrop, font, and hero

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,32 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	// TODO: add  count timer
+	// TODO: add nav bar
+
+	const weddingDate = new Date('2023-04-01');
+
+	const calculateDays = () => {
+		let today = new Date();
+
+		return Math.floor((weddingDate - today) / (1000 * 60 * 60 * 24));
+	};
+</script>
+
+<div class="min-h-screen font-cormorant">
+	<div class="w-fit">
+		<img src="/static/lilac-top.webp" alt="lilac-top" />
+	</div>
+
+	<div class="hero">
+		<div class="hero-content text-center mt-0">
+			<div class="max-w-md">
+				<h1 class="text-5xl ">JOHN & JADE</h1>
+				<p class="pt-6">APRIL 1, 2023 • MANTECA, CA</p>
+				<p class="">{calculateDays()} DAYS TO GO!</p>
+			</div>
+		</div>
+	</div>
+
+	<footer class="footer absolute inset-x-0 bottom-0">
+		<img src="/static/lilac-bottom.webp" alt="lilac-top" />
+	</footer>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,36 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/routes/**/*.{svelte,js,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				cormorant: ['Cormorant', 'serif']
+			}
+		}
+	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#6419E6',
+
+					secondary: '#D926A9',
+
+					accent: '#1FB2A6',
+
+					neutral: '#191D24',
+
+					'base-100': '#FFFFFF',
+
+					info: '#3ABFF8',
+
+					success: '#36D399',
+
+					warning: '#FBBD23',
+
+					error: '#F87272'
+				}
+			}
+		]
+	},
+	plugins: [require('daisyui')]
+};
